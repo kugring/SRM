8629a8eaeeb79939d45e710c22b46cf56459f78c
kugring <wogur8243@gmail.com>
2024-05-10 14:03:03 +0900

    SPA 작업 이전

--- board-front/src/layouts/Footer/index.tsx
+++ board-front/src/layouts/Footer/index.tsx
@@ -4,29 +4,41 @@ import './style.css';
 //          component: 푸터 레이어웃            //
 export default function Footer() {
 
-    //          render: 푸터 레이어웃 렌더링         //
+    //          event handler: 인스타 아이콘 버튼 클릭 이벤트 처리         //
+    const onInstaIconButtonClickHandler = () => {
+        window.open('https://www.instagram.com');
+    }
+
+
+    //          event handler: 네이버 블로그 아이콘 버튼 클릭 이벤트 처리         //
+    const onNaverBlogIconButtonClickHandler = () => {
+        window.open('https://www.naver.com');
+    }
     return (
-        <div className='footer-container'>
-            <div className='footer-top'>
-                <div className='footer-logo-box'>
-                    <div className='icon-box'>
-                        <div className='logo-light-icon'></div>
+        <div id='footer'>
+            <div className='footer-container'>
+                <div className='footer-top'>
+                    <div className='footer-logo-box'>
+                        <div className='icon-box'>
+                            <div className='icon logo-light-icon'></div>
+                        </div>
+                        <div className='footer-logo-text'>{'Kugring Board'}</div>
                     </div>
-                    <div className='footer-logo-text'></div>
+                    <div className='footer-link-box'>
+                        <div className='footer-email-link'>{'wogur8243@naver.com'}</div>
+                        <div className='icon-button' onClick={onInstaIconButtonClickHandler}>
+                            <div className='icon insta-icon'></div>
+                        </div>
+                        <div className='icon-button' onClick={onNaverBlogIconButtonClickHandler}>
+                            <div className='icon naver-blog-icon'></div>
+                        </div>
+                    </div>
                 </div>
-                <div className='footer-link-box'>
-                    <div className='footer-email-link'></div>
-                    <div className='icon-button'>
-                        <div className='icon insta-icon'></div>
-                    </div>
-                    <div className='icon-button'>
-                        <div className='icon naver-blog-icon'></div>
-                    </div>
+                <div className='footer-bottom'>
+                    <div className='footer-copyright'>{'Copyright © 2024 Kugring. All Rights Reserved.'}</div>
                 </div>
-            </div>
-            <div className='footer-bottom'>
-                <div className='footer-copyright'></div>
             </div>
         </div>
+
     )
 }
