10a5221a6d51d5d2947be1ad409de4668724cd02
kugring <wogur8243@gmail.com>
2024-06-07 14:32:23 +0900

    댓글작성일자 수정함

--- board-back/src/main/java/com/kugring/boardback/repository/CommentRePository.java
+++ board-back/src/main/java/com/kugring/boardback/repository/CommentRePository.java
@@ -15,13 +15,13 @@ public interface CommentRePository extends JpaRepository<CommentEntity, Integer>
         "SELECT " + 
         "    U.nickname AS nickname, " +
         "    U.profile_image AS profileImage, " +
-        "    C.write_datetime AS writerDatetime, " +
+        "    C.write_datetime AS writeDatetime, " +
         "    C.content AS content " +
         "FROM comment AS C " +
         "INNER JOIN user AS U " +
         "ON C.user_email = U.email " +
         "WHERE C.board_number = ?1 " +
-        "ORDER BY writerDatetime DESC ",
+        "ORDER BY writeDatetime DESC ",
         nativeQuery = true
     )
     List<GetCommentListResultSet> getCommentList(Integer boardNumber);
